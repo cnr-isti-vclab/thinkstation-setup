75216d34c5251edfa721fdc43320837b0da1eb64
alemuntoni <muntoni.alessandro@gmail.com>
2026-04-24 10:52:26 +0200

    add homepage

--- setup.sh
+++ setup.sh
@@ -11,4 +11,10 @@ jq -r '.[]' images.json | while IFS= read -r image; do
     docker pull "$image"
 done
 
+# get the current machine's IP address to pass to the homepage container
+THINKSTATION_IP=$(hostname -I | awk '{print $1}')
+
+# create a .env file with the THINKSTATION_IP variable for docker-compose to use
+echo "THINKSTATION_IP=${THINKSTATION_IP}" > .env
+
 docker compose up -d
